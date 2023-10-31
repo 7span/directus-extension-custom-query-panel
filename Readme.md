0e7624fb977ba49abd9871ff5ff0d6594738823f
Bhagyesh Radiya <bhagyesh@7span.com>
2023-10-31 09:20:03 +0530

    Updated the readme file and optimized the code.

--- Readme.md
+++ Readme.md
@@ -1,8 +1,48 @@
-# Custom Query Panel
+# Directus Custom Query Panel
 
-An easy to use insight panel to see custom raw query data from database
+Behold the magic 🎩✨ of our simple panel! View your data without the hassle of writing custom endpoints or preparing views - it's like having your cake 🍰 and eating it too, but for data! 💾🎉
 
-# 👀 Must Check
+
+## Details
+
+- Execute custom SQL queries directly from the panel.
+- Use dynamic parameters to reuse queries with different values.
+- Display query results in a simple tabular structure for easy readability.
+- Enhance data visibility and understanding by enabling users to extract meaningful insights from their data which is crucial for CRM and data-driven applications.
+
+
+## 👀Set Up Instructions
+
+- Either install through NPM [npm install @7span/directus-extension-custom-query-panel](https://www.npmjs.com/) or [Installing Through the Extensions Folder](https://docs.directus.io/extensions/installing-extensions.html#installing-through-the-extensions-folder).
+- Once Installed/configured extension you need hit the below curl request to create a default table(`cqp_queries`) to store the queries.
+
+**NOTE**: Replace `localhost:8055` with your domain
+
+```bash
+curl --location --request POST 'http://localhost:8055/query/create-table'
+```
+
+-   Table named `cqp_queries` will be available in your project.
+
+## How To use This Extension
+
+-  Create the queries in the table (`cqp_queries`) as explained in below example.
+
+For Example: 
+```bash
+select first_name, last_name from employees where department = ${department} 
+```
+
+-  This extension provides support of *`global variables`* added in insights like department or week.
+![Department Example](/images/department-panel.png)
+
+-  Use `variables` field give in panel settings below `fields`.
+![dynamic-fields](/images/dynamic-fields.png)
+
+-  Use `double mustache` syntax for entering `value` field to get value from variables.
+![Alt text](/images/double-mustache.png)
+
+## 👀 Must Check
 
 -   Query response must be in array format
 -   Query field should have `code` interface with minimum length of 1000 characters
@@ -18,10 +58,6 @@ An easy to use insight panel to see custom raw query data from database
 ## Extension Type
 
 -   📦 Bundle ( Panel + Custom Endpoint )
-
-## Set Up Instructions
-
-{ Include any instructions required to set up this submission - including any registrations for third-party services. }
 
 ## Screenshots
 
@@ -43,27 +79,7 @@ An easy to use insight panel to see custom raw query data from database
 -   this extension uses raw query. Use with caution. It might do uninteded actions.
 -   Roles and permission check for query
 
-## How To use This Extension
-
--   Once you install the extension into your directus project by `npm install`
--   Hit the below curl request to create a default table to store the queries.
-
-NOTE: Replace `localhost:8055` with your domain
-
-```bash
-curl --location --request POST 'localhost:8055/query/create-table'
-```
-
--   Table named `cqp_queries` will be available in your project.
--   Create the queries in the table.
--   Provide the query id where the vue component is available.
-
-### Global Variables Support
-
--   This extension provides support of global variables added in insights like department or week.
--   Use `variables` field give in panel settings below `fields`.
--   Use `double mustache` syntax for entering `value` field to get value from variables.
-
 ### Table Fields
 
 -   We have repeater interface with multiple columns support
+
--- src/custom-query/index.js
+++ src/custom-query/index.js
@@ -5,35 +5,23 @@ export default {
   handler: (router, { services, logger, database }) => {
     const { ItemsService } = services;
     const tableName = "cqp_queries";
+
+    // Endpoint to execute a custom query
     router.get("/execute", async (req, res) => {
       try {
-        const customQuery = new ItemsService(tableName, { schema: req.schema });
+        // Initialize the custom query service
+        const customQueryService = new ItemsService(tableName, { schema: req.schema });
         const { query_id } = req.query;
-        // Retrieving the query for the collection based on the ID.
-        const customQueryData = await customQuery.readOne(query_id);
 
-        const queryVariables = req.query.variables
-        const expectedData = {}
-        // To validate the request conditional fields for the query.
-        queryVariables.forEach((item) => {
-          const { key, value } = item;
-          // Use typeof to check the type of the value
-          console.log('typeof value', value, typeof value)
-          if (isInteger(value)) {
-            // If it's a integer, convert it to a integer type
-            expectedData[key] = `${parseInt(value)}`;
-          } else if(isFloat(value)){
-             // If it's a float, convert it to a float type
-            expectedData[key] = `${parseFloat(value)}`;
-          } else if (typeof value === "string") {
-            // If it's a string, keep it as a string
-            expectedData[key] = `'${value}'`;
-          } else {
-            console.log(`Unsupported data type for key ${key}`)
-          }
-        });
-        const query = phrase(`${customQueryData.query}`, expectedData);
-        console.log('Custom Query details for testing----', query)
+        // Retrieve the custom query based on the provided ID
+        const customQueryData = await customQueryService.readOne(query_id);
+
+        // Prepare the variables for the custom query
+        const queryVariables = req.query.variables;
+        const preparedVariables = prepareVariables(queryVariables);
+
+        // Execute the custom query
+        const query = phrase(`${customQueryData.query}`, preparedVariables);
         if (query) {
           const executedQueryData = await database.raw(query);
           const fetchedQueryData = executedQueryData[0];
@@ -46,33 +34,41 @@ export default {
         logger.error(`error: ${error}`);
         return res.status(500).json({ error: `${error.message}` });
       }
-    }),
-    // This endpoint will create a default structure for storing the query details
-      router.post("/create-table", async (req, res) => {
-        try {
-          await database.schema.createTable(tableName, (table) => {
-            table.increments(), table.string("name"), table.string("query");
+    });
+
+    // Endpoint to create a table for storing custom queries
+    router.post("/create-table", async (req, res) => {
+      try {
+        await database.schema.createTable(tableName, (table) => {
+          table.increments(), table.string("name"), table.string("query");
+        });
+        return res
+          .status(200)
+          .json({
+            message: `Table Created for Custom Query Extension ${tableName}`,
           });
-          return res
-            .status(200)
-            .json({
-              message: `Table Created for Custom Query Extension ${tableName}`,
-            });
-        } catch (error) {
-          logger.error(`error: ${error}`);
-          return res.status(500).json({ error: `${error.message}` });
-        }
-      });
+      } catch (error) {
+        logger.error(`error: ${error}`);
+        return res.status(500).json({ error: `${error.message}` });
+      }
+    });
   },
 };
 
-function isInteger(value) {
-  // Use a regex pattern to check for an integer
-  const integerPattern = /^\d+$/;
-  return integerPattern.test(value);
-}
-function isFloat(value) {
-  // Use a regex pattern to check for an float number
-  const floatPattern = /^-?\d*\.?\d+$/;
-  return floatPattern.test(value);
+// Function to prepare the variables for the custom query 
+function prepareVariables(variables) {
+  const preparedVariables = {};
+  variables.forEach((item) => {
+    const { key, value } = item;
+    if (Number.isInteger(value)) {
+      preparedVariables[key] = `${parseInt(value)}`;
+    } else if (!isNaN(parseFloat(value))) {
+      preparedVariables[key] = `${parseFloat(value)}`;
+    } else if (typeof value === "string") {
+      preparedVariables[key] = `'${value}'`;
+    } else {
+      console.log(`Unsupported data type for key ${key}`)
+    }
+  });
+  return preparedVariables;
 }
